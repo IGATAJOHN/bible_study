5e7192e1d1fdedd4b60dca9eb8cba26a4eb77ae5
IGATAJOHN <144548770+IGATAJOHN@users.noreply.github.com>
2025-04-15 14:21:49 +0100

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,1 +1,23 @@
+const CACHE_NAME = 'bible-study-v1';
+const ASSETS = [
+    '/',
+    '/static/css/style.css',
+    '/static/js/app.js',
+    '/static/icons/icon-192x192.png',
+    '/static/icons/icon-512x512.png',
+    '/static/img/bible.jfif'
+];
 
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
